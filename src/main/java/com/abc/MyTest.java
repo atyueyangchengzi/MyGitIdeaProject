1d52725aea1541e6ab5cb1d5fb5f884c0a5b7412
ljc <1685683003@qq.com>
2022-02-03 18:02:24 +0800

    idea second

---
+++ src/main/java/com/abc/MyTest.java
@@ -0,0 +1,16 @@
+package com.abc;
+
+/**
+ * ClassName:MyTest
+ * Package:com.abc
+ * Description:
+ *
+ * @Date: 2022/2/3 18:00
+ * @Author: Liujc
+ */
+
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("MyTest");
+    }
+}
