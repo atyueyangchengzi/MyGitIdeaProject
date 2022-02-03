512cced62320708e7624eaf50dca408c00e4d317
ljc <1685683003@qq.com>
2022-02-03 18:34:50 +0800

    hot-fix01 second

---
+++ src/main/java/org/example/MyTest001.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * ClassName:MyTest001
+ * Package:org.example
+ * Description:
+ *
+ * @Date: 2022/2/3 18:21
+ * @Author: Liujc
+ */
+
+public class MyTest001 {
+    public static void main(String[] args) {
+        System.out.println("MyTest001");
+    }
+}
---
+++ src/main/java/org/example/MyTest02.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * ClassName:MyTest02
+ * Package:org.example
+ * Description:
+ *
+ * @Date: 2022/2/3 18:13
+ * @Author: Liujc
+ */
+
+public class MyTest02 {
+    public static void main(String[] args) {
+        System.out.println("MyTest02");
+    }
+}
